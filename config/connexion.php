85b45215da20b11a7949551716b0b8efe81ba920
laila el meknouni <laila.elmeknouni@usmba.ac.ma>
2022-11-28 14:26:39 +0100

    first commit

---
+++ config/connexion.php
@@ -0,0 +1,16 @@
+<?php
+
+try {
+		$access=new pdo("mysql:host=localhost;dbname=ecomm;charset=utf8", "root", "");
+		
+		$access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+} catch (Exception $e) 
+{
+	$e->getMessage();
+}
+    
+    
+
+
+?>
